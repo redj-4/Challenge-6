444ee0a9284268667fe84b3aaa6aa4096111fd04
Divya Joshi <divyaj.redj@gmail.com>
2025-02-09 19:17:06 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -17,3 +17,29 @@ const calculateSalesTax = function(amount, taxRate) {
 // Test Data:
 calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
 calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
+
+//Task 3: Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+  let bonusPercentage = 0;
+  
+  switch (performanceRating) {
+    case "Excellent":
+      bonusPercentage = 0.20;
+      break;
+    case "Good":
+      bonusPercentage = 0.10;
+      break;
+    case "Average":
+      bonusPercentage = 0.05;
+      break;
+    default:
+      bonusPercentage = 0;
+  }
+  
+  const bonus = salary * bonusPercentage;
+  return `Bonus: $${bonus}`;
+};
+
+// Test Data
+console.log(calculateBonus(5000, "Excellent")); // Expected output: "Bonus: $1000"
+console.log(calculateBonus(7000, "Good"));      // Expected output: "Bonus: $700"
