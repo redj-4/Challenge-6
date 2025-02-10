77865fd1e9a6621a4c5d59793b1b37d40447fae1
Divya Joshi <divyaj.redj@gmail.com>
2025-02-09 19:05:13 -0500

    Task 1 - Business Profit Calculation.

---
+++ cc_6.js
@@ -0,0 +1,9 @@
+//Task 1: Function Declaration 
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    const profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`);
+  }   //function declaration for calculating total profit
+
+  // Test Data:
+calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
+calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
