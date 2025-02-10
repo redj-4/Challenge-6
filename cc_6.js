598d3b5cf3ad41e5d313c884a4310582a4102519
Divya Joshi <divyaj.redj@gmail.com>
2025-02-09 19:10:11 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
   // Test Data:
 calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
 calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
+
+//Task 2: Function Expression 
+const calculateSalesTax = function(amount, taxRate) {
+  const salesTax = amount * taxRate;
+  console.log(`Sales Tax: $${salesTax}`);
+};    // Declare a function expression for calculating sales tax
+
+// Test Data:
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
+calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
